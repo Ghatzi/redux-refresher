7e86da74f704752a063a6058e9c9d25af1f2b428
G Hatzi <52458189+Ghatzi@users.noreply.github.com>
2023-02-15 12:32:40 +0000

    add rtk-query

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { apiSlice } from './features/api/apiSlice';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApiProvider api={apiSlice}>
+      <App />
+    </ApiProvider>
   </React.StrictMode>
 );
